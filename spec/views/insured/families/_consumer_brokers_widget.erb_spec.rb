409fa467d33179b8acb9e491cc02f494eb316ac4
sudhirreddysama <sudhirsama586@gmail.com>
2017-11-14 11:29:05 -0500

    ref#19687 error fixed something went wrong

--- spec/views/insured/families/_consumer_brokers_widget.erb_spec.rb
+++ spec/views/insured/families/_consumer_brokers_widget.erb_spec.rb
@@ -43,9 +43,6 @@ RSpec.describe "_consumer_brokers_widget.html.erb" do
       f.family_members = [
         FactoryGirl.build(:family_member, family: f, person: person, is_primary_applicant: true)
       ]
-      f.broker_agency_accounts = [
-        FactoryGirl.build(:broker_agency_account, family: f)
-      ]
       f.save
       f
     end
@@ -54,7 +51,6 @@ RSpec.describe "_consumer_brokers_widget.html.erb" do
     before :each do
       assign(:person, person)
       assign :family_members, [family_member]
-      allow(person).to receive_message_chain("primary_family.current_broker_agency.present?").and_return(false)
       render 'insured/families/consumer_brokers_widget'
     end
 
@@ -69,7 +65,41 @@ RSpec.describe "_consumer_brokers_widget.html.erb" do
     it "should display get help signing up button" do
       expect(rendered).to have_selector('a', "Find Assistance Another Way")
     end
+  end
+  context 'insured home broker widget as consumer with broker agency and without broker' do
+    let!(:consumer_role) { FactoryGirl.create(:consumer_role) }
+    let(:person) { consumer_role.person }
+    let!(:family) do
+      f = FactoryGirl.build(:family)
+      f.family_members = [
+        FactoryGirl.build(:family_member, family: f, person: person, is_primary_applicant: true)
+      ]
+      f.broker_agency_accounts = [
+        FactoryGirl.build(:broker_agency_account, family: f, writing_agent_id: nil)
+      ]
+      f.save
+      f
+    end
+    let(:family_member) { family.family_members.last }
 
+    before :each do
+      assign(:person, person)
+      assign :family_members, [family_member]
+      allow(person).to receive_message_chain("primary_family.current_broker_agency.writing_agent").and_return(nil)
+      render 'insured/families/consumer_brokers_widget'
+    end
+
+    it "should display broker widget for consumer" do
+      expect(rendered).to have_selector('h3', "Select a Broker or Assister")
+    end
+
+    it "should display get help signing up button" do
+      expect(rendered).to have_selector('a', "Get Help Signing Up")
+    end
+
+    it "should display get help signing up button" do
+      expect(rendered).to have_selector('a', "Find Assistance Another Way")
+    end
   end
 
 end
